373e52c909083919e99a3fe7bc5f37e0f0fbef23
Drew B <baddies.censers0o@icloud.com>
2022-09-26 00:09:06 +0000

    Migrate from global packages to local project packages

--- .devcontainer/set-env.js
+++ .devcontainer/set-env.js
@@ -1,20 +1,25 @@
-const globalNPM = "/usr/local/share/npm-global/lib/node_modules";
 const fs = require("fs");
-const localtunnel = require(`${globalNPM}/localtunnel`);
-const dotenv = require(`${globalNPM}/dotenv`);
-const updateDotenv = require(`${globalNPM}/update-dotenv`);
+const localtunnel = require("localtunnel");
+const dotenv = require("dotenv");
+const updateDotenv = require("update-dotenv");
 
 (async () => {
   const envConfig = dotenv.parse(fs.readFileSync('./.env'))
-  if (envConfig.WEBHOOK_PROXY_URL) return;
+  if (envConfig.WEBHOOK_PROXY_URL) {
+    console.log("Webhook already exists, not starting a new localtunnel instance");
+    
+    return;
+  }
 
   const tunnel = await localtunnel({ port: 3000 });
+
+  console.log("Running localtunnel on port 3000 with the url", tunnel.url);
 
   await updateDotenv({
     WEBHOOK_PROXY_URL: tunnel.url
   })
 
   tunnel.on('close', () => {
-    // tunnels are closed
+    console.log("Tunnel was closed");
   });
 })();
